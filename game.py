ed1dc6cff1cffc92a18a824b198a73e22f1ad75c
wareaglee <kobialvarez90@gmail.com>
2021-06-23 10:56:39 -0400

    clean up and random implement
    Cleaned up quite a few lines of where the game talked to the user making it seem more realistic as well as fixed minor typo's. i then implemented the random function so the game isn't so static there is now a random feature to the health's and attacks of both you and your enemy.

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 #this is a game where you have to try to defeat Zorlock the wizard 
 
+import random
+
 username = input('Enter a username : ') 
 print(f"Welcome {username.lower()}! ")
 player_health = 100
@@ -13,45 +15,48 @@ while enemy_health > 0 :
         attack_decision = input("Attack enemy? (y/n) : ")
 
         if attack_decision.lower() == 'y':
-            player_health = player_health - 35
-            enemy_health = enemy_health - 45
-            print(f"[!] Your close range attck let {enemy_name} get a good hit on you, your health is now {player_health} and you dealt enough damage to the enemy that his health is now {enemy_health}")
+            player_health = player_health - random.randint(15, 35)
+            enemy_health = enemy_health - random.randint(20, 45)
+            print(f"[!] Your close range attck let {enemy_name} get a good hit on you, your health is now {player_health} and you dealt enough damage to make his health {enemy_health}")
             continue
         else:
-            player_health = player_health - 35
-            print(f"{username} didnt' act fast enough {enemy_name} attacked you for 35 making your new health {player_health}. ") 
+            player_health = player_health - random.randint(5, 40)
+            print(f"[!]{username} didnt' act fast enough {enemy_name} attacked you, making your new health {player_health}. ") 
             continue
+
     elif weapon.lower() == "staff":
         print(f"You have selcted: {weapon.lower()}")
         attack_decision = input("Attack enemy? (y/n) : " )
 
         if  attack_decision.lower() == 'y':
-            player_health = player_health - 15
-            enemy_health = enemy_health - 25
-            print(f"[!] Your staff makes your reach longer so you did not take much damage your new health is {player_health} and your enemey is now at {enemy_health}")
+            player_health = player_health - random.randint(10, 25)
+            enemy_health = enemy_health - random.randint(10, 19)
+            print(f"[!] Your staff makes your reach longer, you didn't take much damage your health is now {player_health} and zorlocks is now at {enemy_health}")
             continue
         else:
-            player_health = player_health - 10
-            print(f"{username} didnt act fast enough {enemy_name} was able to hit you.. {username}, your new heal is {player_health}.")
+            player_health = player_health - random.randint(11, 24)
+            print(f"[!]{username} didnt act fast enough! {enemy_name} was able to hit you.. {username}, your health is {player_health}.")
             continue
+
     elif weapon.lower() == "shield":
         print(f"You have selcted: {weapon.lower()}")
         attack_decision = input("Attack enemy? (y/n) : " )
 
         if attack_decision.lower() == "y":
-            player_health = player_health - 5
-            enemy_health = enemy_health - 10
-            print(f"{username}, your shield is durable but not strong in damage. you dealt 10 damage bringing {enemy_name} to {enemy_health}. He hit you for 5 bringing you to {player_health}")
+            player_health = player_health - random.randint(0, 10)
+            enemy_health = enemy_health - random.randint(10, 15)
+            print(f"[!]{username}, your shield is durable but not strong in damage. {enemy_name}'s health is now {enemy_health}.Good thing you had a shield he wasnt able to do much damage. Your health is {player_health}")
             continue
         else: 
-            player_health = player_health + 5
-            print(f"{username} used the shield to reflect {enemy_name}'s attack. Good Job your health is now {player_health}")
+            player_health = player_health + random.randint(10, 21)
+            print(f"[!]{username} used the shield to reflect {enemy_name}'s attack. Good Job your health is now {player_health}")
             continue
     else:
-        print("you need to choose a weapon in order to defeat", enemy_name)
+        print("You need to choose a weapon in order to defeat", enemy_name)
         continue
+
 if enemy_health <= 0: 
     print(f"Good job {username} you have defeated {enemy_name}!!")
 
 if player_health <=0:
-    print("Unlucky you have gotten defeated by Zorlock... Better luck next time.")
+    print(f"Unlucky you have gotten defeated by {enemy_name}... Better luck next time.")
